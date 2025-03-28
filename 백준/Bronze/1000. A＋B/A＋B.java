74d9ef27e5dbde713d712fcc7cd3dc9c5d42026c
K-KY <kyuyoungk@naver.com>
2025-03-28 18:41:58 +0900

    [Bronze V] Title: A+B, Time: 172 ms, Memory: 17688 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String[] ab = sc.nextLine().split(" ");
+        
+        System.out.println(Integer.parseInt(ab[0]) + Integer.parseInt(ab[1]));
+    }
+}
