7a168b8671c80226c980c1abcf2cc82eed594842
K-KY <kyuyoungk@naver.com>
2025-04-09 15:10:09 +0900

    [Bronze III] Title: 알람 시계, Time: 208 ms, Memory: 18492 KB -BaekjoonHub

---
+++ 백준/Bronze/2884. 알람 시계/알람 시계.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int h = sc.nextInt();
+        int m = sc.nextInt();
+        
+        m = m - 45;
+        
+        if (m < 0) {
+            m = 60 + m;
+            h -= 1;
+        }
+        
+        if (h < 0) {
+            h = 24 + h;
+        }
+        
+        System.out.println(h + " " + m);
+    }
+}
