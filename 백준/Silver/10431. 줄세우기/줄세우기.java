8e5eaa6a65d682fcf404e8b6452c7b1cf01bedfc
K-KY <kyuyoungk@naver.com>
2025-04-10 20:59:34 +0900

    [Silver V] Title: 줄세우기, Time: 200 ms, Memory: 17756 KB -BaekjoonHub

--- 백준/Silver/10431. 줄세우기/줄세우기.java
+++ 백준/Silver/10431. 줄세우기/줄세우기.java
@@ -1,13 +1,14 @@
 import java.util.*;
+import java.io.*;
 public class Main {
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        int loop = Integer.parseInt(in.nextLine());
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int loop = Integer.parseInt(br.readLine());
         int sb = 0;
         StringBuilder builder = new StringBuilder();
         while (loop-- > 0) {
             sb = 0;
-            int[] arr = Arrays.stream(in.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+            int[] arr = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
             for (int i = 1; i < arr.length; i++) {
                 int count = 0;
                 for (int j = i + 1; j < arr.length; j++) {
