acceee6657bf083225c555c81511d74024fe2793
K-KY <kyuyoungk@naver.com>
2025-04-29 02:19:40 +0900

    [Silver V] Title: 단어 정렬, Time: 668 ms, Memory: 37548 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.java
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.java
@@ -2,23 +2,12 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        Map<Integer, TreeSet<String>> map = new HashMap<>();
+        TreeSet<String> set = new TreeSet<>(Comparator.comparingInt(String::length).thenComparing(String::compareTo));
         int n = in.nextInt();
         for (int i = 0; i < n; i++) {
             String s = in.next();
-            TreeSet<String> treeSet = map.getOrDefault(s.length(), new TreeSet<>());
-            treeSet.add(s);
-            map.putIfAbsent(s.length(), treeSet);
+            set.add(s);
         }
-
-        for (int i = 1; i <= 50; i++) {
-            TreeSet<String> treeSet = map.getOrDefault(i, new TreeSet<>());
-            if (treeSet.isEmpty()) {
-                continue;
-            }
-            for (String s : treeSet) {
-                System.out.println(s);
-            }
-        }
+        set.forEach(System.out::println);
     }
 }
