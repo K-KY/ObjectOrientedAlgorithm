ea2399df67eabdda09fc7fa6066566767b9b0c69
K-KY <kyuyoungk@naver.com>
2025-05-08 11:18:25 +0900

    [Bronze II] Title: 진법 변환, Time: 176 ms, Memory: 17720 KB -BaekjoonHub

---
+++ 백준/Bronze/2745. 진법 변환/진법 변환.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String[] target = sc.nextLine().split(" ");
+        System.out.println(Integer.parseInt(target[0], Integer.parseInt(target[1])));
+    }
+}
