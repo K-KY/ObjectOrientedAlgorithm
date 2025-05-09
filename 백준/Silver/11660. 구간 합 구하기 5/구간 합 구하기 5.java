d44b9daca36a8e3bc4e2d22fcd0a2d446da5eaae
K-KY <kyuyoungk@naver.com>
2025-05-09 22:18:33 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 828 ms, Memory: 125760 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
@@ -1,43 +1,51 @@
-import java.util.*;
 import java.io.*;
+import java.util.StringTokenizer;
 
 public class Main {
-public static void main(String[] args) throws IOException {
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));;
-    int[] fsCs = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-    int frameSize = fsCs[0];
-    int cmdSize = fsCs[1];
-    int[][] frame = new int[frameSize +1][frameSize +1];
-    for (int i = 1; i <= frameSize; i++) {
-        int[] array = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-        for (int j = 1; j <= frameSize; j++) {
-            if (j == 1) {
-                frame[i][j] = array[j - 1];
-                continue;
+    public static void main(String[] args) throws IOException {
+        // 빠른 입력/출력
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        // 입력 행렬 및 누적합 배열 (1-based indexing)
+        int[][] matrix = new int[N + 1][N + 1];
+        int[][] prefix = new int[N + 1][N + 1];
+
+        // 표 입력 받기
+        for (int i = 1; i <= N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 1; j <= N; j++) {
+                matrix[i][j] = Integer.parseInt(st.nextToken());
+                // 누적합 계산
+                prefix[i][j] = matrix[i][j]
+                             + prefix[i - 1][j]
+                             + prefix[i][j - 1]
+                             - prefix[i - 1][j - 1];
             }
-            frame[i][j] = frame[i][j - 1] + array[j - 1];
-
         }
-    }
 
-    for (int i = 1; i <= frameSize; i++) {
-        for (int j = 1; j <= frameSize; j++) {
-            frame[i][j] += frame[i - 1][j];
+        // 질의 처리
+        for (int q = 0; q < M; q++) {
+            st = new StringTokenizer(br.readLine());
+            int x1 = Integer.parseInt(st.nextToken());
+            int y1 = Integer.parseInt(st.nextToken());
+            int x2 = Integer.parseInt(st.nextToken());
+            int y2 = Integer.parseInt(st.nextToken());
+
+            int sum = prefix[x2][y2]
+                    - prefix[x1 - 1][y2]
+                    - prefix[x2][y1 - 1]
+                    + prefix[x1 - 1][y1 - 1];
+
+            bw.write(sum + "\n");
         }
-    }
 
-    for (int i = 0; i < cmdSize; i++) {
-        int[] sXYeXY = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-        int sX = sXYeXY[0];
-        int sY = sXYeXY[1];
-        int eX = sXYeXY[2];
-        int eY = sXYeXY[3];
-
-        int result = frame[eX][eY] - frame[sX - 1][eY] - frame[eX][sY - 1] + frame[sX - 1][sY - 1];
-        bw.write(result+"\n");
+        bw.flush();
+        bw.close();
+        br.close();
     }
-    bw.flush();
 }
-}
-//
