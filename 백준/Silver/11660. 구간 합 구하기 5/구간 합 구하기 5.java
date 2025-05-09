b35133f7658121a027dd09448cab8eea7b8cf7dd
K-KY <kyuyoungk@naver.com>
2025-05-09 22:10:03 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 1212 ms, Memory: 175308 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
@@ -1,38 +1,43 @@
-import java.util.Arrays;
-import java.util.Scanner;
+import java.util.*;
+import java.io.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int frameSize = sc.nextInt();
-        int cmdSize = sc.nextInt();
-        int[][] frame = new int[frameSize +1][frameSize +1];
-        for (int i = 1; i <= frameSize; i++) {
-            for (int j = 1; j <= frameSize; j++) {
-                if (j == 1) {
-                    frame[i][j] = sc.nextInt();
-                    continue;
-                }
-                frame[i][j] = frame[i][j - 1] + sc.nextInt();
-
+public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));;
+    int[] fsCs = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+    int frameSize = fsCs[0];
+    int cmdSize = fsCs[1];
+    int[][] frame = new int[frameSize +1][frameSize +1];
+    for (int i = 1; i <= frameSize; i++) {
+        int[] array = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        for (int j = 1; j <= frameSize; j++) {
+            if (j == 1) {
+                frame[i][j] = array[j - 1];
+                continue;
             }
-        }
+            frame[i][j] = frame[i][j - 1] + array[j - 1];
 
-        for (int i = 1; i <= frameSize; i++) {
-            for (int j = 1; j <= frameSize; j++) {
-                frame[i][j] += frame[i - 1][j];
-            }
-        }
-
-        for (int i = 0; i < cmdSize; i++) {
-            int sX = sc.nextInt();
-            int sY = sc.nextInt();
-            int eX = sc.nextInt();
-            int eY = sc.nextInt();
-
-            int result = frame[eX][eY] - frame[sX - 1][eY] - frame[eX][sY - 1] + frame[sX - 1][sY - 1];
-            System.out.println(result);
         }
     }
+
+    for (int i = 1; i <= frameSize; i++) {
+        for (int j = 1; j <= frameSize; j++) {
+            frame[i][j] += frame[i - 1][j];
+        }
+    }
+
+    for (int i = 0; i < cmdSize; i++) {
+        int[] sXYeXY = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        int sX = sXYeXY[0];
+        int sY = sXYeXY[1];
+        int eX = sXYeXY[2];
+        int eY = sXYeXY[3];
+
+        int result = frame[eX][eY] - frame[sX - 1][eY] - frame[eX][sY - 1] + frame[sX - 1][sY - 1];
+        bw.write(result+"\n");
+    }
+    bw.flush();
+}
 }
 //
