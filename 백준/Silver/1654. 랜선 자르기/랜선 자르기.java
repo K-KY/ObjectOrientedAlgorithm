80e1ecf29419041bcec7646a06266ab3bf57af3b
K-KY <kyuyoungk@naver.com>
2025-05-30 14:21:20 +0900

    [Silver II] Title: 랜선 자르기, Time: 376 ms, Memory: 30724 KB -BaekjoonHub

---
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
@@ -0,0 +1,48 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int has = sc.nextInt();
+        int need = sc.nextInt();
+
+        int[] lines = new int[has];
+
+        for (int i = 0; i < has; i++) {
+            lines[i] = sc.nextInt();
+        }
+        Arrays.sort(lines);
+
+        long i = binarySearch(lines, need);
+
+        System.out.println(i);
+    }
+
+    public static long binarySearch(int[] nums, int target) {
+        long low = 0;
+        long high = Integer.MAX_VALUE;
+        long ans = 0;
+
+        while (low <= high) {
+            long mid = (low + high) / 2;
+            long cut = 0;
+
+            for (int i = 0; i < nums.length; i++) {
+                if (nums[i] >= mid) {
+                    cut += nums[i]/mid;
+                }
+            }
+
+            if (cut < target) {
+                high = mid - 1;
+            }
+
+            if (cut >= target) {
+                ans = mid;
+                low = mid + 1;
+            }
+        }
+        return ans;
+    }
+}
