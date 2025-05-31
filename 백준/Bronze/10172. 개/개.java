bf92ee30c4334045e0ec8c9d263e20768f6b8de8
K-KY <kyuyoungk@naver.com>
2025-05-31 22:31:00 +0900

    [Bronze V] Title: 개, Time: 172 ms, Memory: 17356 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        StringJoiner sj = new StringJoiner("\n");
+        sj.add("|\\_/|");
+        sj.add("|q p|   /}");
+        sj.add("( 0 )\"\"\"\\");
+        sj.add("|\"^\"`    |");
+        sj.add("||_/=\\\\__|");
+        
+        System.out.println(sj);
+      
+    }
+}
