f14dc61a50e16b28246650b0a1b2236bef413cd0
K-KY <kyuyoungk@naver.com>
2025-05-31 22:27:54 +0900

    [Bronze V] Title: 고양이, Time: 164 ms, Memory: 17468 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        StringJoiner sj = new StringJoiner("\n");
+        sj.add("\\    /\\");
+        sj.add(" )  ( \')");
+        sj.add("(  /  )");
+        sj.add(" \\(__)|");
+        System.out.println(sj);
+                
+    }
+}
